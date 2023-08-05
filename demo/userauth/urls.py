2f9742bcf3bf0d15c3023191d601071c28213748
Akshay_Kanawade <kanawade.akshay9@gmail.com>
2023-08-05 18:02:09 +0530

    created route in app

--- demo/userauth/urls.py
+++ demo/userauth/urls.py
@@ -14,11 +14,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path, include
-from userauth import urls
+from userauth import views
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("path/", include("userauth.urls"))
+    path("signup/", views.Register)
 ]
