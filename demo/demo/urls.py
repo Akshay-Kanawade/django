ee5cb7aa22f537bd77013cd137cc15467d7a4e50
Akshay_Kanawade <kanawade.akshay9@gmail.com>
2023-08-27 12:04:11 +0530

    code update

--- demo/demo/urls.py
+++ demo/demo/urls.py
@@ -16,9 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from userauth import urls
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("path/", include("userauth.urls"))
 ]
--- demo/userauth/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-# Create your views here.
-
-
-def Register(request):
-    return HttpResponse("Message----Helllooooo")
